7bf180c4f489fbdbe6cf8583fbb287f508830992
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-11 07:47:12 +0300

    commit js on design

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+
+$(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('').hide('');
+      $("#design").show('');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('');
+      $("#design-image").slideDown('');
+    });
+  });
+
+
+
 $(document).ready(function(){
     $("#development-image").click(function(){
       $("#development-image").slideDown('').hide('');
